1766b6d705cdc14884f547910f52c0265c45ebbb
AkobirToshtemirov <toshtemirovakobir8@gmail.com>
2023-04-02 11:27:34 +0500

    add try catch Exception handler

--- src/ExceptionHandling.java
+++ src/ExceptionHandling.java
@@ -1,3 +1,19 @@
 public class ExceptionHandling {
+    public static void main(String[] args) {
 
+        try {
+            String welcome = "Welcome!";
+            char[] chars = welcome.toCharArray();
+            char lastChar = chars[chars.length - 1];
+            System.out.println("Last Char: " + lastChar);
+
+            String nothing = null;
+            System.out.println(nothing.length());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+        System.out.println("Code ran successfully!");
+
+    }
 }
